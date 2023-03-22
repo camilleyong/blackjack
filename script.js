c7309c01da6f3911830e9db7393bed590f11a375
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-03-21 19:38:24 -0700

    reassgined values

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let arrayCards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let arrayCards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -21,6 +19,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    arrayCards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    isAlive = true
     renderGame()
 }
 
