6fb8132c1b2b4f1e79db5423821b4251ba8fbe36
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2023-03-21 20:18:36 -0700

    added players

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
+
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+let playerEl = document.getElementById("player-el")
+    playerEl.textContent= player.name + ": $" + player.chips
+    
+let player = {
+    name: "Per",
+    chips: 145
+}
+
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
